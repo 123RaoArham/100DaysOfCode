34095c91437e2fcf63d947e9acc3b2beab447d25
Rao Arham <raoarham1234@gmail.com>
2025-09-10 14:22:02 +0530

    Day5 Q9 doneeeeee...

---
+++ Day5-Q9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+      int main(){
+        float p,r,t;
+        float simple_int, compound_int;
+        scanf("%f %f %f", &p,&r,&t);
+        simple_int = (p*r*t)/100;
+        compound_int = (p*pow((1+r/100),t))-p;
+        printf("the Simple intrest = %.2f and compound intrest = %.2f", simple_int,compound_int);
+        return 0;
+        
+        
+      }
